dede52a44791492e728a033cbeab8fd31bbc03e9
David Huie <david@nationbuilder.com>
2013-10-08 16:15:37 -0700

    Correct zip_to_fipz.json path

--- lib/codez.rb
+++ lib/codez.rb
@@ -1,4 +1,9 @@
-module Codez; end
+module Codez
+
+  GEM_PATH = File.dirname(__FILE__)
+  ZIP_TO_FIPS_FILE = File.join(GEM_PATH, 'codez/data/zip_to_fips.json')
+
+end
 
 require "json"
 require_relative "./codez/fips"
--- lib/codez/fips.rb
+++ lib/codez/fips.rb
@@ -4,7 +4,7 @@ class Codez::Fips
     attr_accessor :data
   end
 
-  data_file = File.open("./lib/codez/data/zip_to_fips.json", "r")
+  data_file = File.open(Codez::ZIP_TO_FIPS_FILE, "r")
   self.data = JSON.load(data_file)
 
   def self.from_zip(zip)
